c36b41f00f42363ed75a1455f9cc9456778ccff9
Tomas Detko <tomas@gmail.com>
2024-01-16 23:08:21 +0100

    first input in fform is working

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -29,24 +29,22 @@ const EventForm = ( {userID, type }:EventFormProp ) => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-            
-          )}
-        />
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+        <div className='flex flex-col gap-5 md:flex-row'>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className='w-full'>
+                <FormLabel>Title</FormLabel>
+                <FormControl>               
+                  <Input placeholder="Event title" {...field} className='input-field' />
+                </FormControl>
+                <FormMessage />
+              </FormItem>  
+            )}
+          />
+        </div>
         <Button type="submit">Submit</Button>
       </form>
     </Form>
