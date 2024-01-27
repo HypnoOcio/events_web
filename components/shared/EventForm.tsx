179e87b5ca9618a03d91c9f1d14d9c69e2698db4
Tomas Detko <tomas@gmail.com>
2024-01-27 22:42:18 +0100

    added Category, Description

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -4,22 +4,24 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { FieldValue, FieldValues, useForm } from "react-hook-form";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
-import {Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage} from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { eventFormSchema } from '@/lib/validators';
 import { eventDefaultValues } from '@/constants';
+import Dropdown from './Dropdown';
+import { Textarea } from "@/components/ui/textarea"
 
 export type EventFormProp = {
   userID: string;
   type: "Create" | "Update";
 };
 
-const EventForm = ( {userID, type }:EventFormProp ) => {
+const EventForm = ({ userID, type }: EventFormProp) => {
   const initialValues = eventDefaultValues;
   const form = useForm<z.infer<typeof eventFormSchema>>({
     resolver: zodResolver(eventFormSchema),
     defaultValues: initialValues
-})
+  })
 
   function onSubmit(values: z.infer<typeof eventFormSchema>) {
     // Do something with the form values.
@@ -36,19 +38,45 @@ const EventForm = ( {userID, type }:EventFormProp ) => {
             name="title"
             render={({ field }) => (
               <FormItem className='w-full'>
-                <FormLabel>Title</FormLabel>
-                <FormControl>               
+                <FormControl>
                   <Input placeholder="Event title" {...field} className='input-field' />
                 </FormControl>
                 <FormMessage />
-              </FormItem>  
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem className='w-full'>
+                <FormControl>
+                  <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
             )}
           />
         </div>
+        <div className='flex flex-col gap-5 md:flex-row'>
+          <FormField
+            control={form.control}
+            name='description'
+            render={({ field }) => (
+              <FormItem className='w-full'>
+                <FormControl className='h-72'>
+                  <Textarea placeholder='Description' {...field} className='textarea rounded-2xl'/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default EventForm
+export default EventForm;
